3795d81cbe5e08357eb15e893c04b7c73103fc57
cjia <1902520596@qq.com>
2023-09-03 11:30:49 +0800

    jpa笔记项目依赖引入

--- jpa-note/src/main/java/org/example/JpaApplication.java
+++ jpa-note/src/main/java/org/example/JpaApplication.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * 类描述
+ * jpa项目入口
  *
  * @author cjia
  * @date 2023/9/3 上午 11:17
  */
+@SpringBootApplication
 public class JpaApplication {
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        SpringApplication.run(JpaApplication.class);
     }
 }
