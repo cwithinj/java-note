95f486690c5411a0f2549a159d59f985728ede8a
cjia <1902520596@qq.com>
2023-09-03 10:27:20 +0800

    项目初始化

---
+++ websocket-note/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * websocket项目入口
+ *
+ * @author cjia
+ * @date 2023/9/3 上午 10:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
