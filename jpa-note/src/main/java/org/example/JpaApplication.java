8d7304c650079460a4297e2e4f39e91b02e3a2d6
cjia <1902520596@qq.com>
2023-09-03 11:21:04 +0800

    jpa笔记项目初始化

---
+++ jpa-note/src/main/java/org/example/JpaApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * 类描述
+ *
+ * @author cjia
+ * @date 2023/9/3 上午 11:17
+ */
+public class JpaApplication {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
